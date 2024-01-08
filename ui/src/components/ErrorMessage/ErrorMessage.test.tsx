954771bc9a66d7ee6a46355e4b6bc5f18f613ad7
tobias-gaenzler <1056318+tobias-gaenzler@users.noreply.github.com>
2024-01-08 09:03:25 +0100

    Refactor test

--- ui/src/components/ErrorMessage/ErrorMessage.test.tsx
+++ ui/src/components/ErrorMessage/ErrorMessage.test.tsx
@@ -2,14 +2,14 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import ErrorMessage from "./ErrorMessage";
 import { ERROR_MESSAGE } from "./ErrorMessage";
 
+const restartFunction = jest.fn();
 describe("testing ErrorMessage", () => {
   test("renders Error Message", () => {
-    render(<ErrorMessage restart={function (): void { }} />);
+    render(<ErrorMessage restart={restartFunction} />);
     const errorMessage = screen.getByText(new RegExp(ERROR_MESSAGE, "i"));
     expect(errorMessage).toBeInTheDocument();
   });
   test("calls callback on click on restart", () => {
-    const restartFunction = jest.fn();
     render(<ErrorMessage restart={restartFunction} />);
     const restartButton = screen.getByRole("button");
     fireEvent.click(restartButton);
